1de5b6f16f7502c8be46c2244183063108563d4c
Brent Feuerbacher <feuerbacherb@gmail.com>
2020-08-05 13:56:01 -0500

    created authentication with tokens

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -13,7 +14,8 @@ const app = express();
 // create a new Apollo server and pass in the schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate the Apollo server with the Express application as middleware
